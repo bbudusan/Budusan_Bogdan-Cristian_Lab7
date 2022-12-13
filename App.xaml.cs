dfc96ad0248c77b99190c40cec470e58a358717c
bbudusan <bbudusan@yahoo.com>
2022-12-13 16:08:43 +0200

    .

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,27 @@
-﻿namespace Budusan_Bogdan_Cristian_Lab7;
+﻿using System;
+using Budusan_Bogdan_Cristian_Lab7;
+using System.IO;
+using Budusan_Bogdan_Cristian_Lab7.Data;
+
+namespace Budusan_Bogdan_Cristian_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
